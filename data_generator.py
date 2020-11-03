9d6e6d04d737a8e1434f191e3e812eea5a6171ec
Abel Barrera Duran <aduran@Aduran-Macbook.local>
2020-11-03 09:13:02 -0500

    number now drawn!

--- data_generator.py
+++ data_generator.py
@@ -3,6 +3,7 @@ import numpy as np
 from imageio import imwrite
 import cv2
 import random
+from PIL import Image
 
 # MNIST_Data_Load = DownloadMNISTData(fullData=False, trainAmt=10, testAmt=10)
 
@@ -20,6 +21,18 @@ cv2.line(img=img, pt1=(0, 100), pt2=(
 
 latest_location = 100
 
+digit_img = cv2.imread('./data/MNIST/jpg_form/test/00000.jpg')
+
+digit_img = cv2.bitwise_not(digit_img)
+
+digit_img_PIL = Image.fromarray(np.uint8(digit_img)).convert(
+    'RGB')  # Image.open(digit_img)
+
+img_PIL = Image.fromarray(np.uint8(img)).convert(
+    'RGB')  # Image.open(img)
+
+img_PIL.paste(digit_img_PIL, (50, 20))
+
 # above we see we AT A MINIMUM need to give 100 y-diff to be able to draw a circle and put the image
 
-imwrite("Result.jpg", img)
+imwrite("Result.jpg", img_PIL)

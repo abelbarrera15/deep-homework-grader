5fbf47be7fe38c3170beee203623c1a97b449065
Abel Barrera Duran <aduran@Aduran-Macbook.local>
2020-11-03 16:27:24 -0500

    homework image now created

--- homework_maker.py
+++ homework_maker.py
@@ -1,8 +1,10 @@
 
 
 class Homework():
-    def __init__(self, numberOfHomeworks: int):
-        self.numberOfHomeworks = numberOfHomeworks
+    def __init__(self, numberOfHomeworks_Test: int, numberOfHomeworks_Train: int):
+        self.numberOfHomeworks_Test = numberOfHomeworks_Test
+        self.numberOfHomeworks_Train = numberOfHomeworks_Train
 
     def createHomework(self):
-        for index in (range(self.numberOfHomeworks + 1)):
+        for index in (range(self.numberOfHomeworks_Test + 1)):
+            
